f433bc26cfe0308bb970fec29b4d6d864f5e83e9
RMCrean <RMCrean@users.noreply.github.com>
2025-05-15 17:16:09 +0200

    .bucket_versions.yaml file stays in bucket all times
    Instead of having a local copy of the file that tracks the bucket version, just keep in the s3 bucket.
    
    Whenever a command is run, get latest version from the bucket.
    If modification/creation command, then the version in the bucket is updated.
    
    Hopefully reduce chances of version conflicts/collisions.

--- experiments/version_entire_bucket/set_bucket_version.py
+++ experiments/version_entire_bucket/set_bucket_version.py
@@ -1,132 +1,151 @@
 """
 Manage versions of the entire bucket
 
-This script is interacted with via argparse. 
+This script is interacted with via argparse.
 
-Versioning specified by updating a yaml file in the bucket called ".bucket_versions.yaml".
+Versioning specified by updating a file in the bucket called ".bucket_versions.yaml".
 
-Versioning done by setting the version according to the current timestamp.  
+Versioning done by setting the version according to the current timestamp.
+The version file is always kept in the bucket, not stored on disk.
 """
-from dataclasses import dataclass, field
-import yaml
+
 import argparse
-from pathlib import Path
+import os
+from dataclasses import dataclass, field
 from datetime import datetime, timezone
 
-BUCKET_VERSION_FILE = ".bucket_versions.yaml"
+import boto3
+import botocore
+import yaml
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+
+VERSION_FILE_NAME = ".bucket_versions.yaml"
+MINIO_URL = "api.divbase-testground.scilifelab-2-dev.sys.kth.se"
+
+ACCESS_KEY = os.getenv("MINIO_SQUIRREL_USER_ACCESS_KEY")
+SECRET_KEY = os.getenv("MINIO_SQUIRREL_USER_SECRET_KEY")
+
 
 @dataclass
 class BucketVersionManager:
-    base_dir: Path
-    # these 2 are not defined till post_init method
-    version_file: Path = field(init=False)
+    bucket_name: str
+    # not defined till post_init method run.
+    s3_client: "botocore.client.S3" = field(init=False)
     version_info: dict = field(init=False)
 
     def __post_init__(self):
-        self.base_dir = Path(self.base_dir).resolve()
-        self.version_file = self.base_dir / BUCKET_VERSION_FILE
-        self.version_info = {}
+        load_dotenv()
+        self.s3_client = boto3.client(
+            "s3",
+            endpoint_url=f"https://{MINIO_URL}",
+            aws_access_key_id=ACCESS_KEY,
+            aws_secret_access_key=SECRET_KEY,
+        )
 
-        if not self.version_file.exists():
-            return
+        # empty dict if file doesn't exist
+        self.version_info = self._get_bucket_version_file()
 
-        with self.version_file.open("r") as f:
-            self.version_info = yaml.safe_load(f)
-            print(f"Loaded version info from file: {self.version_file.resolve()}")
+        if self.version_info:
+            print(f"Version file found inside Bucket: {self.bucket_name}.")
 
     def create_metadata_file(self):
         """Create the initial metadata file with a default version."""
-        if self.version_file.exists():
-            print(f"Version file already exists at {self.version_file.resolve()}.")
+        if self.version_info:
+            print(f"Can't create a new version file as one already exists in the bucket: {self.bucket_name}.")
             return
 
         timestamp = self._create_timestamp()
-        content = {
-            "versions": {
-                "v0.1.0": {
-                    "timestamp": timestamp,
-                    "description": "Initial version"
-                }
-            }
-        }
+        version_data = {"versions": {"v0.1.0": {"timestamp": timestamp, "description": "Initial version"}}}
 
-        self._write_to_file(content)
-        print(f"Metadata file created at {self.version_file.resolve()}.")
+        self._upload_bucket_version_file(version_data=version_data)
+        print("Bucket versioning file created successfully.")
 
-    def add_version(self, name: str, description: str):
+    def add_version(self, name: str, description: str | None) -> None:
         """Add a new version to the metadata file."""
-        if not self.version_file.exists():
-            print("No version file found in this directory, ensure you're in the correct place.")
-            return
-        
         version_data = self.version_info
 
-        # Add the new version
         timestamp = self._create_timestamp()
-        version_data["versions"][name] = {
-            "timestamp": timestamp,
-            "description": description
-        }
+        if not description:
+            description = ""
+        version_data["versions"][name] = {"timestamp": timestamp, "description": description}
 
-        self._write_to_file(version_data)
+        self._upload_bucket_version_file(version_data=version_data)
         # only update the version_info if the file was successfully written
         self.version_info = version_data
-        print(f"Version {name} added to version file at {self.version_file.resolve()}.")
 
     def list_versions(self):
-
-        # TODO - think about this logic of 
-        # checking for both file existence and version info in the class
-        if not self.version_file.exists():
-            print("No version file found in this directory, ensure you're in the correct place.")
-            return
         if not self.version_info:
-            print("No versioning information present as of yet")
+            print("No bucket level versioning has been created for this bucket as of yet.")
             return
 
-        print("Versions in the metadata file:")
+        print("Bucket versions:")
+        # TODO - perhaps user friendly timestamp format too?
         for version, details in self.version_info["versions"].items():
             print(f"- {version}: {details['timestamp']} ({details['description']})")
 
     def _create_timestamp(self) -> str:
         return datetime.now(tz=timezone.utc).isoformat()
 
-    def _read_from_file(self) -> dict:
-        with self.version_file.open("r") as f:
-            return yaml.safe_load(f)
+    def _get_bucket_version_file(self) -> dict:
+        """
+        If bucket version files exists, download version metadata file exists in the S3 bucket.
+        If doesn't exist, return empty dict.
+        """
+        try:
+            response = self.s3_client.get_object(Bucket=self.bucket_name, Key=VERSION_FILE_NAME)
+        except self.s3_client.exceptions.NoSuchKey:
+            print(f"The version file was not found in bucket: {self.bucket_name}, hopefully this is correct...")
+            return {}
 
-    def _write_to_file(self, content: dict):
-        with self.version_file.open("w") as f:
-            yaml.safe_dump(content, f, default_flow_style=False)
+        content = response["Body"].read().decode("utf-8")
+        # TODO - how would this handle an empty file, aka file exists but is empty?
+        return yaml.safe_load(content)
+
+    def _upload_bucket_version_file(self, version_data: dict) -> None:
+        """
+        Upload a version metadata file to the S3 bucket.
+        Works for both creating and updating the file.
+        """
+        try:
+            self.s3_client.put_object(Bucket=self.bucket_name, Key=VERSION_FILE_NAME, Body=yaml.safe_dump(version_data))
+            print(f"New version updated in the bucket: {self.bucket_name}.")
+        except ClientError as e:
+            print(f"Failed to upload version file: {e}")
 
 
 def create_command(args):
-    manager = BucketVersionManager(args.base_dir)
+    manager = BucketVersionManager(args.bucket_name)
     manager.create_metadata_file()
 
+
 def add_command(args):
-    manager = BucketVersionManager(args.base_dir)
+    manager = BucketVersionManager(args.bucket_name)
     manager.add_version(args.name, args.description)
 
+
 def list_command(args):
-    manager = BucketVersionManager(args.base_dir)
+    manager = BucketVersionManager(args.bucket_name)
     manager.list_versions()
 
-def delete_command(args):
-    pass
 
+def delete_command(args):
+    # TODO should this even be a command?
+    pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage bucket versions")
 
     parent_parser = argparse.ArgumentParser(add_help=False)
-    parent_parser.add_argument("--base_dir", default=".", help="Base directory for the version metadata file.")
+    parent_parser.add_argument("--bucket_name", default=".", help="Name of the storage bucket for the project.")
 
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # Subcommand: create
-    create_parser = subparsers.add_parser("create", parents=[parent_parser], help="Create the versioning metadata file.")
+    create_parser = subparsers.add_parser(
+        "create", parents=[parent_parser], help="Create the versioning metadata file."
+    )
     create_parser.set_defaults(func=create_command)
 
     # Subcommand: add
