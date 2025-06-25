4b8458ade07e1a3587174c24448e4df03d4ef5aa
Daniel P Brink <daniel.brink@scilifelab.se>
2025-06-25 11:25:20 +0200

    Drop example task that was used for testing Celery

--- src/divbase_tools/tasks.py
+++ src/divbase_tools/tasks.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from time import sleep
 
 from celery import Celery
 
@@ -21,19 +20,6 @@ app.conf.update(
 )
 
 logger = logging.getLogger(__name__)
-
-
-@app.task(name="tasks.add")
-def add(x, y):
-    """
-    A Hello World-like task to learn how to run tasks in Celery.
-    """
-    i = 0
-    while i < 5:
-        sleep(1)
-        print("processing...")
-        i += 1
-    return x + y
 
 
 @app.task(name="tasks.bcftools_pipe")
