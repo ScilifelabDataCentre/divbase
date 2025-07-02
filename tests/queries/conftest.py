90e0c99410debf77cb4c5c2763e59d1317ccb392
Daniel P Brink <daniel.brink@scilifelab.se>
2025-07-02 11:08:25 +0200

    Make pytest spin up testing env for job system
    The override compose.yaml changes host ports to avoid conflicts with the
    main job system stack, if that happens to also be running on the same
    machine. Tempfs is used for the redis and flower logs, which allows for
    testing of logs during the test session while still purging them after
    the session ends.
    
    Another key addition is a helper function that ensures that the docker
    compose health checks are successful before job system tests are run.

--- tests/queries/conftest.py
+++ tests/queries/conftest.py
@@ -1,14 +1,50 @@
+import os
 import shutil
 import tempfile
 import time
 from pathlib import Path
 from typing import Any
+from unittest.mock import patch
 
 import pandas as pd
 import pytest
 from celery import current_app
 
 from divbase_tools.queries import BcftoolsQueryManager, SidecarQueryManager
+from tests.helpers.job_system_setup import start_job_system, stop_job_system
+
+
+@pytest.fixture(autouse=True, scope="session")
+def set_celery_env_vars():
+    """
+    Set environment variables for Celery broker and result backend to run in the
+    testing docker stack defined in job_system_compose_tests.yaml.
+    Separated from job_system_docker_stack fixture to ensure that these env variables are
+    used in all tests that require Celery, even if the job system stack was started outside of pytest.
+    """
+    os.environ["CELERY_BROKER_URL"] = "pyamqp://guest@localhost:5673//"
+    os.environ["CELERY_RESULT_BACKEND"] = "redis://localhost:6380/0"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def job_system_docker_stack():
+    """
+    Start job system docker stack, and stop after all tests run.
+    (The print message is only displayed when pytest is run with the -s option)
+    """
+    try:
+        start_job_system()
+        yield
+    finally:
+        stop_job_system()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def patch_container_name():
+    """Patch the container name for BcftoolsQueryManager to look for the testing stack container name when testing synchronous tasks"""
+    # TODO update this when we do the planned renaming of the docker-compose file in the future
+    with patch("divbase_tools.queries.BcftoolsQueryManager.CONTAINER_NAME", "divbase-job-system-tests-worker-1"):
+        yield
 
 
 @pytest.fixture
