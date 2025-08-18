fb3f475f7118fac2ca3f64def8c6c0bdf88b9156
Daniel P Brink <daniel.brink@scilifelab.se>
2025-08-18 15:41:27 +0200

    Skip transfer of unused VCFs for 'view -r' jobs
    When running queries that include `bcftools view -r`, the system will
    now check the dimension index file to see if there are files that do not
    contain the regions that will be filtered on. If so, these files will
    not be transferred from the bucket to the worker. This will potentially
    save a lot of resources when the bucket contains large VCF files.
    
    Example:
    Init a clean compose stack
    
    docker compose -f docker/divbase_compose.yaml down -v && \
    docker compose -f docker/divbase_compose.yaml up --build -d; \
    python scripts/local_dev_setup.py
    
    if this query is run when the bucket does not contain
    .vcf_dimensions.yaml, it will transfer all VCF found by the metadata tsv
    query to the worker:
    
    DIVBASE_ENV=local divbase-cli query bcftools-pipe
    --tsv-filter "Area:West of Ireland;Sex:F" \
    --command "view -s SAMPLES; view -r 1,4,6,21,24" \
    --metadata-tsv-name sample_metadata_HOM_chr_split_version.tsv \
    --project local-project-3
    
    But if this command has been run before to create the dimensions file,
    the task will identify potential VCFs from the metadata tsv query that
    do not contain the regions that will be filtered on,
    and skip transferring them:
    
    DIVBASE_ENV=local divbase-cli dimensions update \
    --project local-project-3

--- src/divbase_tools/tasks.py
+++ src/divbase_tools/tasks.py
@@ -1,8 +1,10 @@
 import dataclasses
 import logging
 import os
+import re
 from pathlib import Path
 
+import yaml
 from celery import Celery
 
 from divbase_tools.queries import BCFToolsInput, BcftoolsQueryManager, run_sidecar_metadata_query
@@ -100,17 +102,31 @@ def bcftools_pipe_task(
         filter_string=tsv_filter,
     )
 
+    if "view -r" in command:
+        files_to_download = check_for_unnecessary_files_for_region_query(
+            bucket_name=bucket_name,
+            s3_file_manager=s3_file_manager,
+            command=command,
+            files_to_download=metadata_result.unique_filenames,
+        )
+
+        sample_and_filename_subset = [
+            entry for entry in metadata_result.sample_and_filename_subset if entry["Filename"] in files_to_download
+        ]
+    else:
+        sample_and_filename_subset = metadata_result.sample_and_filename_subset
+
     _ = download_vcf_files(
-        files_to_download=metadata_result.unique_filenames,
+        files_to_download=files_to_download,
         bucket_name=bucket_name,
         s3_file_manager=s3_file_manager,
     )
 
     bcftools_inputs = dataclasses.asdict(
         BCFToolsInput(
-            sample_and_filename_subset=metadata_result.sample_and_filename_subset,
+            sample_and_filename_subset=sample_and_filename_subset,
             sampleIDs=metadata_result.unique_sample_ids,
-            filenames=metadata_result.unique_filenames,
+            filenames=files_to_download,
         )
     )
 
@@ -152,7 +168,7 @@ def update_vcf_dimensions_task(bucket_name: str, user_name: str = "Default User"
             return {"status": "error", "error": str(e), "task_id": task_id}
     return {"status": "completed", "submitter": user_name}
     # TODO return some meaningful status message. it should perhaps say which files that were indexed?
-    # TODO delete downloaded files upon fininshing, can use delete_job_files_from_worker() from vcf_benchmarking branch
+    # TODO delete downloaded files upon fininshing
 
 
 def download_sample_metadata(metadata_tsv_name: str, bucket_name: str, s3_file_manager: S3FileManager) -> Path:
@@ -186,3 +202,65 @@ def upload_results_file(output_file: Path, bucket_name: str, s3_file_manager: S3
         to_upload={output_file.name: output_file},
         bucket_name=bucket_name,
     )
+
+
+def check_for_unnecessary_files_for_region_query(
+    bucket_name: str, s3_file_manager: S3FileManager, command: str, files_to_download: list[str]
+) -> dict:
+    """
+    If the 'view -r' query is present, read the .vcf_dimensions.yaml file and check if the specified scaffolds are available in the VCF files.
+    If a file in files_to_download (as identified by sidecar sample metadata query) does not contain any of the specified scaffolds, it will be skipped.
+    This will save time and resources by avoiding unnecessary transfer of irrelevant files between the bucket and the worker.
+
+    NOTE! There is a risk that users mistype...
+
+    TODO ensure that the errors raised here are raised before submitting the task to celery.
+    """
+
+    try:
+        dimensions_index = read_vcf_dimensions_file(bucket_name=bucket_name, s3_file_manager=s3_file_manager)
+    except FileNotFoundError:
+        return files_to_download
+        # TODO for now, this makes it so that if the dimensions file is not present, the job continues by skipping the check of unnecessary files. There should at least be a message that tells this to the user
+
+    scaffolds = []
+    files_to_download_updated = []
+    matches = re.findall(r"view\s+-r\s+([^\s;]+)", command)
+    for match in matches:
+        scaffolds.extend([region.strip() for region in match.split(",") if region.strip()])
+
+    for file in files_to_download:
+        record = None
+        for rec in dimensions_index.get("dimensions", []):
+            if rec.get("filename") == file:
+                record = rec
+                break
+        if not record:
+            print(f"File '{file}' is not indexed in the VCF dimensions file.")
+            continue
+
+        record_scaffolds = set(record.get("dimensions", {}).get("scaffolds", []))
+        for scaffold in scaffolds:
+            if scaffold in record_scaffolds:
+                print(f"Scaffold '{scaffold}' is present in file '{file}'.")
+                if file not in files_to_download_updated:
+                    files_to_download_updated.append(file)
+
+    if files_to_download_updated == []:
+        raise ValueError(
+            "Based on the 'view -r' query and the VCF scaffolds indexed in DivBase, there are no VCF files in the project that fulfills the query. Please try another -r query with scaffolds/chromosomes that are present in the VCF files."
+        )
+        # TODO this stops jobs from executing. Which might not be intuitive. Such subsests would be able to run in bftools but the result would be an merged VCF with no variants (header only). Should this be a warning instead that will be returned in the worker results?
+
+    return files_to_download_updated
+
+    # TODO when there is a command implemented that reads the .vcf_dimensions.yaml file and list all scaffolds in the project, add that command as a help to this error message.
+
+
+def read_vcf_dimensions_file(bucket_name: str, s3_file_manager: S3FileManager) -> dict:
+    try:
+        content = s3_file_manager.download_s3_file_to_str(key=".vcf_dimensions.yaml", bucket_name=bucket_name)
+    except Exception as exc:
+        raise FileNotFoundError(f".vcf_dimensions.yaml not found in bucket '{bucket_name}'.") from exc
+    # TODO add a hint to the error telling how dimension indexing can be done.
+    return yaml.safe_load(content)
