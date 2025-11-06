2c9a0b8c710d78ab69228a7b92f3efc76c1ace84
Daniel P Brink <daniel.brink@scilifelab.se>
2025-11-06 10:51:20 +0100

    Silence sync db engine logging

--- packages/divbase-api/src/divbase_api/worker/worker_db.py
+++ packages/divbase-api/src/divbase_api/worker/worker_db.py
@@ -11,5 +11,5 @@ from sqlalchemy.orm import sessionmaker
 logger = logging.getLogger(__name__)
 
 WORKER_DATABASE_URL = os.getenv("WORKER_DATABASE_URL", "NOT_SET")
-sync_engine = create_engine(WORKER_DATABASE_URL, echo=True)
+sync_engine = create_engine(WORKER_DATABASE_URL, echo=False)
 SyncSessionLocal = sessionmaker(bind=sync_engine)
