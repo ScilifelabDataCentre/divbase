57b0d40594265fc8ca64048394ed817a5da4457e
RMCrean <RMCrean@users.noreply.github.com>
2025-11-27 14:29:17 +0100

    remove requirment for api to have worker db url

--- packages/divbase-api/src/divbase_api/worker/worker_db.py
+++ packages/divbase-api/src/divbase_api/worker/worker_db.py
@@ -10,6 +10,15 @@ from sqlalchemy.orm import sessionmaker
 
 logger = logging.getLogger(__name__)
 
-WORKER_DATABASE_URL = os.getenv("WORKER_DATABASE_URL", "NOT_SET")
-sync_engine = create_engine(WORKER_DATABASE_URL, echo=False, pool_pre_ping=True)
-SyncSessionLocal = sessionmaker(bind=sync_engine)
+# if block is used as API and worker are in same package and
+# API imports modules from the worker (e.g. tasks.py).
+# Without the if:
+# - Would have to add the WORKER_DATABASE_URL environment variable to the API and
+# - create the SyncSessionLocal object that would not used by the API.
+# TODO - consider creating SyncSessionLocal object using https://docs.celeryq.dev/en/stable/userguide/signals.html#worker-init instead?
+WORKER_DATABASE_URL = os.getenv("WORKER_DATABASE_URL")
+if WORKER_DATABASE_URL:
+    sync_engine = create_engine(WORKER_DATABASE_URL, echo=False, pool_pre_ping=True)
+    SyncSessionLocal = sessionmaker(bind=sync_engine)
+else:
+    SyncSessionLocal = None
