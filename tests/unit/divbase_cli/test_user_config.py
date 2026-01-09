b2e329da04c96a90f1ffa2cc4a4367576f89f911
RMCrean <RMCrean@users.noreply.github.com>
2026-01-09 10:28:57 +0100

    update test for user has no config file
    to catch the new custom exception in prior commit

--- tests/unit/divbase_cli/test_user_config.py
+++ tests/unit/divbase_cli/test_user_config.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 import pytest
 
+from divbase_cli.cli_exceptions import ConfigFileNotFoundError
 from divbase_cli.user_config import ProjectNotInConfigError, create_user_config, load_user_config
 
 
 def test_show_user_config_fails_if_no_config_file(tmp_path: Path) -> None:
     """Test that the show command fails if no config file exists."""
 
-    with pytest.raises(FileNotFoundError):
+    with pytest.raises(ConfigFileNotFoundError):
         load_user_config(config_path=tmp_path / "non_existent_config.yaml")
 
 
