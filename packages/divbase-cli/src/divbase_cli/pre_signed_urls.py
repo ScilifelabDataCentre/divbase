71378cfd94794c0bc6336a997ab20b308cc51320
RMCrean <RMCrean@users.noreply.github.com>
2026-01-19 08:43:05 +0100

    client side multipart download logic

--- packages/divbase-cli/src/divbase_cli/pre_signed_urls.py
+++ packages/divbase-cli/src/divbase_cli/pre_signed_urls.py
@@ -5,6 +5,7 @@ TODO: Consider adding retries, error handling, progress bars, etc.
 """
 
 import logging
+from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -15,6 +16,11 @@ from divbase_lib.exceptions import ChecksumVerificationError
 from divbase_lib.s3_checksums import verify_downloaded_checksum
 
 logger = logging.getLogger(__name__)
+
+# Used for multipart file downloads
+CHUNK_SIZE = 1024 * 1024 * 8  # 8MB
+MAX_CONCURRENCY = 8
+MULTIPART_DOWNLOAD_THRESHOLD = CHUNK_SIZE * 2
 
 
 @dataclass
@@ -52,13 +58,19 @@ def download_multiple_pre_signed_urls(
     successful_downloads, failed_downloads = [], []
     with httpx.Client(timeout=30.0) as client:
         for obj in pre_signed_urls:
-            result = _download_single_pre_signed_url(
-                httpx_client=client,
-                pre_signed_url=obj.pre_signed_url,
-                verify_checksums=verify_checksums,
-                output_file_path=download_dir / obj.name,
-                object_name=obj.name,
-            )
+            output_file_path = download_dir / obj.name
+            object_name = obj.name
+            try:
+                result = _download_single_pre_signed_url(
+                    httpx_client=client,
+                    pre_signed_url=obj.pre_signed_url,
+                    verify_checksums=verify_checksums,
+                    output_file_path=output_file_path,
+                    object_name=object_name,
+                )
+            except httpx.HTTPError as err:
+                result = FailedDownload(object_name=object_name, file_path=output_file_path, exception=err)
+
             if isinstance(result, SuccessfulDownload):
                 successful_downloads.append(result)
             else:
@@ -72,19 +84,23 @@ def _download_single_pre_signed_url(
 ) -> SuccessfulDownload | FailedDownload:
     """
     Download a single file using a pre-signed URL.
+    If the file is large enough, we download in chunks.
     Helper function, do not call directly from outside this module.
     """
-    with httpx_client.stream("GET", pre_signed_url) as response:
-        try:
-            response.raise_for_status()
-        except httpx.HTTPError as err:
-            return FailedDownload(object_name=object_name, file_path=output_file_path, exception=err)
+    content_length, server_checksum = _get_content_length_and_checksum(
+        httpx_client=httpx_client, pre_signed_url=pre_signed_url
+    )
 
-        server_checksum = response.headers.get("ETag", "").strip('"')
+    if content_length < MULTIPART_DOWNLOAD_THRESHOLD:
+        _perform_singlepart_download(
+            httpx_client=httpx_client,
+            pre_signed_url=pre_signed_url,
+            output_file_path=output_file_path,
+        )
 
-        with open(output_file_path, "wb") as file:
-            for chunk in response.iter_bytes(chunk_size=8192):
-                file.write(chunk)
+    else:
+        logger.info(f"Starting multipart download for large file '{object_name}' of size {content_length} bytes.")
+        _perform_multipart_download(httpx_client, pre_signed_url, output_file_path, content_length)
 
     if verify_checksums:
         try:
@@ -93,6 +109,76 @@ def _download_single_pre_signed_url(
             return FailedDownload(object_name=object_name, file_path=output_file_path, exception=err)
 
     return SuccessfulDownload(file_path=output_file_path, object_name=object_name)
+
+
+def _get_content_length_and_checksum(httpx_client: httpx.Client, pre_signed_url: str) -> tuple[int, str]:
+    """
+    "HEAD" a pre-signed download URL to get it's content length and checksum.
+
+    As you can't HEAD a presigned GET, we do a GET with a Range header to only get the first byte.
+    Otherwise would have to be given a separate pre-signed HEAD url to do this.
+    """
+    with httpx_client.stream("GET", pre_signed_url, headers={"Range": "bytes=0-0"}) as head_response:
+        head_response.raise_for_status()
+        content_range = head_response.headers["Content-Range"]
+        # format is "bytes 0-0/12345"
+        content_length = int(content_range.split("/")[-1])
+        server_checksum = head_response.headers["ETag"].strip('"')
+    return content_length, server_checksum
+
+
+def _perform_singlepart_download(httpx_client: httpx.Client, pre_signed_url: str, output_file_path: Path) -> None:
+    """Used on objects smaller than the multipart threshold cutoff"""
+    with httpx_client.stream("GET", pre_signed_url) as response:
+        response.raise_for_status()
+
+        with open(output_file_path, "wb") as file:
+            for chunk in response.iter_bytes(chunk_size=8192):
+                file.write(chunk)
+
+
+def _perform_multipart_download(httpx_client, pre_signed_url, output_file_path, content_length):
+    """
+    Download a large file in multiple chunks using range requests.
+
+    As we write to the file concurrently, the file is first created with the correct size,
+    and then each chunk is written to the correct position in the file.
+    """
+    with open(output_file_path, "wb") as f:
+        f.seek(content_length - 1)
+        f.write(b"\0")
+    with ThreadPoolExecutor(max_workers=MAX_CONCURRENCY) as executor:
+        futures = []
+        for i in range(0, content_length, CHUNK_SIZE):
+            start = i
+            end = min(i + CHUNK_SIZE, content_length)
+            futures.append(
+                executor.submit(
+                    _download_chunk,
+                    httpx_client,
+                    pre_signed_url,
+                    start,
+                    end,
+                    output_file_path,
+                )
+            )
+
+        for future in futures:
+            future.result()
+
+
+def _download_chunk(client: httpx.Client, url: str, start: int, end: int, output_file_path: Path) -> None:
+    """
+    Downloads a specific range of bytes of a file (aka chunk),
+    and writes it to the correct place in the file.
+    """
+    headers = {"Range": f"bytes={start}-{end - 1}"}
+    with client.stream("GET", url, headers=headers) as response:
+        response.raise_for_status()
+        with open(output_file_path, "rb+") as f:
+            f.seek(start)
+            for chunk in response.iter_bytes():
+                f.write(chunk)
 
 
 @dataclass
