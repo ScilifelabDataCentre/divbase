ae688aa52ea65136dd48bdfad156c49bdd24ef5e
RMCrean <RMCrean@users.noreply.github.com>
2026-04-15 09:31:20 +0200

    cli: drop no longer used code

--- packages/divbase-cli/src/divbase_cli/config_resolver.py
+++ packages/divbase-cli/src/divbase_cli/config_resolver.py
@@ -68,28 +68,6 @@ def resolve_project(project_name: str | None) -> ProjectConfig:
     return config.project_info(project_name)
 
 
-def resolve_divbase_api_url(url: str | None) -> str:
-    """
-    Helper function to resolve the DivBase API URL to use for a CLI command.
-
-    If not provided by the user, it will take the default project's API URL from the user config.
-    Otherwise raise an error.
-    """
-    if url:
-        return url
-
-    config = load_user_config()
-
-    if not config.default_project:
-        raise ValueError(
-            "No default project is set in your user config. "
-            "Please set a default project or specify the API URL using the --url option."
-        )
-
-    project_config = config.project_info(name=config.default_project)
-    return project_config.divbase_url
-
-
 def resolve_download_dir(download_dir: str | None) -> Path:
     """
     Helper function to resolve the download directory to use for a CLI command involving downloading files.
