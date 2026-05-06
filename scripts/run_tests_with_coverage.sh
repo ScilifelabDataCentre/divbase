8d88c016bff365bf79f50821008ab4f253afda71
Daniel P Brink <daniel.brink@scilifelab.se>
2026-05-06 14:23:05 +0200

    Use toml for the coverage config instead of ini

--- scripts/run_tests_with_coverage.sh
+++ scripts/run_tests_with_coverage.sh
@@ -7,7 +7,7 @@
 # The script runs pytest with the --coverage-docker flag to trigger this mode. Since pytest invoke tests that use the CLI to speak to the
 # other services in the stack, there will also be a host .coverage file generated for the part of the code that is run on the host machine duriung the test session. 
 # After pytest completes, the script copies the host .coverage file into docker/coverage-data/ and runs 'coverage combine' to merge it with the coverage data from the containers. 
-# Finally, the script generates an HTML report with paths remapped to the local source tree using the [paths] config in .coveragerc.
+# Finally, the script generates an HTML report with paths remapped to the local source tree using the [paths] config in .coveragerc.toml.
 #
 # Usage:
 #   ./scripts/run_tests_with_coverage.sh
