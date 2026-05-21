173f5ba4c22929d9dd88af71653b259300569f02
Daniel P Brink <daniel.brink@scilifelab.se>
2026-05-21 16:52:56 +0200

    Drop test_poll_task_retry.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,20 +14,8 @@ def pytest_addoption(parser):
         help="Run slow tests",
     )
     parser.addoption(
-        "--run-very-slow",
-        action="store_true",
-        default=False,
-        help="Run tests marked very-slow (may take several minutes). Also runs tests marked slow.",
-    )
-    parser.addoption(
         "--coverage-docker",
         action="store_true",
         default=False,
         help="Enable running test coverage analysis for all tests, including those that run code inside docker containers. Do not use this flag directly, use the `scripts/run_tests_with_coverage.sh` to do coverage analysis",
     )
-
-
-def pytest_configure(config):
-    """When --run-very-slow is set, also enable --run-slow."""
-    if config.getoption("--run-very-slow", default=False):
-        config.option.run_slow = True
