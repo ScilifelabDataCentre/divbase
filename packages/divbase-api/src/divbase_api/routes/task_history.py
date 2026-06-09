54c426c53d789270b1df9d50b1b7a058be81b488
RMCrean <RMCrean@users.noreply.github.com>
2026-06-09 15:49:33 +0200

    api: drop special read user check in task history endpoint
    By removing we end up with an empty table which is expected/matches slurm behaviour, and no user would have been able to submit a job without first having non read role, so unlikely to ever be raised

--- packages/divbase-api/src/divbase_api/routes/task_history.py
+++ packages/divbase-api/src/divbase_api/routes/task_history.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing_extensions import Annotated
 
-from divbase_api.crud.projects import check_if_user_is_not_only_read_user_in_all_their_projects, has_required_role
+from divbase_api.crud.projects import has_required_role
 from divbase_api.crud.task_history import get_tasks_pg
 from divbase_api.db import get_db
 from divbase_api.deps import get_project_member, require_task_history_scope
@@ -23,8 +23,6 @@ logger = structlog.get_logger(__name__)
 
 task_history_router = APIRouter()
 
-READ_USER_ERROR_MSG = "You do not have access view to task history from any projects. You need to have at least one project where you have an EDIT role or higher."
-
 
 @task_history_router.get("/tasks/user", status_code=status.HTTP_200_OK, response_model=list[TaskHistoryResult])
 async def get_all_tasks_for_user(
@@ -32,7 +30,8 @@ async def get_all_tasks_for_user(
     db: AsyncSession = Depends(get_db),
 ) -> list[TaskHistoryResult]:
     """
-    Get the task history for the current user. Admin users can view all tasks (even if not member of the projects), non-admin users can only view their own tasks.
+    Get the task history for the current user.
+    Admin users can view all tasks (even if not member of the projects), non-admin users can only view their own tasks.
     """
 
     serialized_tasks = await get_tasks_pg(
@@ -40,19 +39,7 @@ async def get_all_tasks_for_user(
         user_id=current_user.id,
         is_admin=current_user.is_admin,
     )
-    result = deserialize_tasks_to_result(serialized_tasks)
-
-    if not result:
-        user_has_at_least_one_edit_role = await check_if_user_is_not_only_read_user_in_all_their_projects(
-            db=db,
-            user_id=current_user.id,
-        )
-
-        if not user_has_at_least_one_edit_role:
-            raise AuthorizationError(READ_USER_ERROR_MSG)
-
-    # result.user_email = current_user.email
-    return result
+    return deserialize_tasks_to_result(serialized_tasks)
 
 
 @task_history_router.get(
@@ -71,7 +58,7 @@ async def get_all_tasks_for_user_and_project(
 
     if not has_required_role(role, ProjectRoles.EDIT):
         raise AuthorizationError(
-            "Project not found or you don't have permission to view task history from this project."
+            "Project not found or you don't have permission to view task history for this project."
         )
 
     serialized_tasks = await get_tasks_pg(
@@ -80,9 +67,7 @@ async def get_all_tasks_for_user_and_project(
         project_id=project.id,
         is_admin=current_user.is_admin,
     )
-    result = deserialize_tasks_to_result(serialized_tasks)
-    # result.user_email = current_user.email
-    return result
+    return deserialize_tasks_to_result(serialized_tasks)
 
 
 @task_history_router.get(
@@ -135,5 +120,4 @@ async def get_task_by_id(
     if not serialized_task:
         raise AuthorizationError("Task ID not found or you don't have permission to view the history for this task ID.")
 
-    result = deserialize_tasks_to_result(serialized_task)
-    return result
+    return deserialize_tasks_to_result(serialized_task)
--- tests/e2e_integration/cli_commands/test_task_history_cli.py
+++ tests/e2e_integration/cli_commands/test_task_history_cli.py
@@ -328,14 +328,6 @@ def test_read_user_cannot_see_task_history(CONSTANTS, logged_in_read_user_with_e
     """
     Integration test that read user cannot access the task history, since they cannot submit tasks.
     """
-
-    # Without --project flag, error is about being an only-Read user
-    result_history = runner.invoke(app, "task-history user")
-    assert result_history.exit_code == 1
-    assert "authorization_error" in str(result_history.exception)
-    assert "You do not have access view to task history" in str(result_history.exception)
-
-    # With --project flag, error is about access to this particular project
     project_name = CONSTANTS["QUERY_PROJECT"]
     result_history = runner.invoke(app, f"task-history user --project {project_name}")
     assert result_history.exit_code == 1
